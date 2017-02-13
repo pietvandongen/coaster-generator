7356294a8736da15dede1deef50ca773c88e6056
Piet van Dongen <piet.vandongen@luminis.eu>
2017-02-13 21:28:59 +0100

    Cut away polygons not in inner circle, made code more efficient (see #1)

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ var paths = {
 };
 
 var isProductionBuild = !!util.env.isProductionBuild;
+console.log(isProductionBuild);
 
 gulp.task("build", gulp.series(
     cleanBuild,
--- src/typescript/application.ts
+++ src/typescript/application.ts
@@ -2,32 +2,36 @@ import * as $ from "jquery";
 import * as d3Array from "d3-array";
 import * as d3Selection from "d3-selection";
 import * as d3Voronoi from "d3-voronoi";
-import * as d3Polygon from "d3-polygon";
+import {VoronoiLayout} from "d3-voronoi";
 
-let graphContainer = $(".graph");
-let height = Math.min(graphContainer.height(), graphContainer.width());
-let width = height;
-let svg = d3Selection.select(".graph")
+let numberOfPolygonsIncludingEdgePieces: number = 66;
+let graphContainer: JQuery = $(".graph");
+let height: number = Math.min(graphContainer.height(), graphContainer.width());
+let width: number = height;
+let circleCenterX: number = Math.round(width / 2);
+let circleCenterY: number = Math.round(height / 2);
+let circleRadius: number = Math.floor(width / 2) - 1;
+
+let coaster: any = d3Selection.select(graphContainer.get(0))
     .append("svg")
-    .attr("width", width)
-    .attr("height", height);
-let sites = d3Array.range(100)
-    .map(function (d) {
-        return [Math.random() * width, Math.random() * height];
-    });
-let voronoi = d3Voronoi.voronoi()
+    .attr("height", height)
+    .attr("width", width);
+
+let sites: number[][] = d3Array.range(numberOfPolygonsIncludingEdgePieces)
+    .map(() => [Math.random() * width, Math.random() * height]);
+
+let voronoi: VoronoiLayout<number[]> = d3Voronoi.voronoi()
     .extent([[-1, -1], [width + 1, height + 1]]);
-let polygon = svg.append("g")
+
+coaster
+    .append("g")
     .attr("class", "polygons")
     .selectAll("path")
     .data(voronoi.polygons(sites))
     .enter()
     .append("path")
-    .call(redrawPolygon);
+    .call((polygon: any) => polygon.attr("d", (points: number[][]) => pointsAreAllInCircle(points) ? "M" + points.join("L") + "Z" : null));
 
-function redrawPolygon(polygon) {
-    polygon
-        .attr("d", function (d) {
-            return d ? "M" + d.join("L") + "Z" : null;
-        });
+function pointsAreAllInCircle(points: number[][]): boolean {
+    return points.every(point => Math.pow(point[0] - circleCenterX, 2) + Math.pow(point[1] - circleCenterY, 2) < Math.pow(circleRadius, 2));
 }
